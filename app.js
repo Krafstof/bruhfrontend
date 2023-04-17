e1ac322fd84792a130e8ad6e8f771f476d5d34aa
andkrav <andrey.kravtsov@graebert.com>
2023-04-17 20:27:53 +0300

    apitest

---
+++ app.js
@@ -0,0 +1,12 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+let mainwindowTitleContainer = document.getElementById("mainwindowTitleContainer");
+
+let p = document.createElement("p");
+
+p.innerText = `Hi, ${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`
+
+mainwindowTitleContainer.appendChild(p);
