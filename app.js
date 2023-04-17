6a296d4cd691711b8e8fb31927fa5ba3f360bcd3
andkrav <andrey.kravtsov@graebert.com>
2023-04-17 23:48:23 +0300

    dfsasf

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let mainwindowTitleContainer = document.getElementById("mainwindowTitleContainer
 
 let p = document.createElement("p");
 
-p.innerText = `Hi, ${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`
+p.innerText = `Привет, ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`
 
 mainwindowTitleContainer.appendChild(p);
