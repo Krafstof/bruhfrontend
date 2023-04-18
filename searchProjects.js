46db6f0a8a7810dd43c631dd2ba85dbcb42f0fc4
andkrav <andrey.kravtsov@graebert.com>
2023-04-19 00:01:03 +0300

    iframe

--- searchProjects.js
+++ searchProjects.js
@@ -1,8 +1,13 @@
 let tg = window.Telegram.WebApp;
 var BackButton = tg.BackButton;
 
+let mainwindowTitleContainer = document.getElementById("clickcker");
+mainwindowTitleContainer.addEventListener('click', () => {
+  javascript:history.back();
+});
+
 BackButton.addEventListener('click', () => {
-    javascript:history.back()
+    javascript:history.back();
   });
 
 BackButton.show();
