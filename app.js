2f7033f4ebfbe3b986879ae8fd571ac13173bbf0
andkrav <andrey.kravtsov@graebert.com>
2023-04-19 18:28:23 +0300

    asdfsad

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let tg = window.Telegram.WebApp;
 var BackButton = tg.BackButton;
+const mainButton = Telegram.WebApp.MainButton;
+
+mainButton.show();
+BackButton.show();
 
 BackButton.addEventListener('click', () => {
     javascript:history.back();
   });
-
-BackButton.show();
